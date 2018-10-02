a568ec6644c5f5008fd67a73327153ffecf07b9f
EC2 Default User <ec2-user@ip-172-31-24-246.us-east-2.compute.internal>
2018-10-02 02:27:36 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,9 +9,44 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
+  
 
   def index
-    @movies = Movie.all
+    @list_ratings = {'G' => '0','PG' => '1', 'PG-13' => '2', 'R' => '3'}
+    @all_ratings = Movie.all_ratings
+    if params[:ratings].nil?
+      if session[:ratings].nil?
+        @selected_ratings = @all_ratings
+        session[:ratings] = @selected_ratings
+      else
+        @selected_ratings = session[:ratings]
+      end
+    else
+      @selected_ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    end
+  
+    
+    if params[:sort].nil?
+      if session[:sort].nil?
+        sort_option = Movie.all
+        session[:sort] = sort_option
+      else
+        sort_option = session[:sort]
+      end
+    else
+      sort_option = params[:sort]
+      session[:sort] = params[:sort]
+    
+    end
+    if sort_option == 'title'
+      @title_header = 'hilite'
+      @movies = Movie.where(:rating => @selected_ratings.keys).order(title: :asc)
+    end
+    if sort_option == 'release_date'  
+      @release_header = 'hilite'
+      @movies = Movie.where(:rating => @selected_ratings.keys).order(release_date: :asc)
+    end
   end
 
   def new
@@ -41,5 +76,4 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
 end
